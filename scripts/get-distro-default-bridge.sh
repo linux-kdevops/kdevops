775c73037bb03ce321e6ecced22533d72fa33f11
Chuck Lever <chuck.lever@oracle.com>
2024-04-30 20:57:08 -0400

    get-distro-default-bridge.sh: Set specific IP address for Fedora
    Fedora appears to use 192.168.122.1 for both guestfs and vagrant.
    
    Reviewed-by: Luis Chamberlain <mcgrof@kernel.org>
    Signed-off-by: Chuck Lever <chuck.lever@oracle.com>

--- scripts/get-distro-default-bridge.sh
+++ scripts/get-distro-default-bridge.sh
@@ -8,6 +8,12 @@ VIRT_TYPE=$2
 # is if you are using vagrant or guestfs. If your distribution needs
 # a different deafult it should be easy for you to extend it here.
 
+DISTRO_FEDORA=`scripts/os-release-check.sh fedora`
+if [[ "$DISTRO_FEDORA" == "y" ]]; then
+	echo "192.168.122.1"
+	exit 0
+fi
+
 if [[ "$VIRT_TYPE" == "vagrant" ]]; then
 	echo "192.168.124.1"
 else
