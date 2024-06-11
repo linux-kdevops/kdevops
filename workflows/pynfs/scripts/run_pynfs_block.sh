976a7592fbacbbbb6a4aef0386a6eb094d4e7a9f
Chuck Lever <chuck.lever@oracle.com>
2024-06-11 13:17:39 -0400

    pynfs: Enable testing with pNFS block layouts
    When this new Kconfig option is enabled, the pynfs workflow runs the
    pNFS block layout tests.
    
    Signed-off-by: Chuck Lever <chuck.lever@oracle.com>

---
+++ workflows/pynfs/scripts/run_pynfs_block.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+version="4.1"
+
+cd ${PYNFS_DATA}/nfs${version}
+./testserver.py --json="${PYNFS_DATA}/pynfs-block-results.json" --maketree --uid=0 --gid=0 "${EXPORT_BASE}-${version}" block
