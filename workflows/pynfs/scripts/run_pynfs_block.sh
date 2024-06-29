7e1b8bfdd7af57099eda9440ef4890981280af7b
Chuck Lever <chuck.lever@oracle.com>
2024-06-29 19:51:39 -0400

    pynfs: Use xfs when testing pNFS
    The pNFS block tests have to use xfs for its export no matter what
    the default NFSD export fstype is.
    
    Signed-off-by: Chuck Lever <chuck.lever@oracle.com>

--- workflows/pynfs/scripts/run_pynfs_block.sh
+++ workflows/pynfs/scripts/run_pynfs_block.sh
@@ -3,4 +3,4 @@
 version="4.1"
 
 cd ${PYNFS_DATA}/nfs${version}
-./testserver.py --json="${PYNFS_DATA}/pynfs-block-results.json" --maketree --uid=0 --gid=0 "${EXPORT_BASE}-${version}" block
+./testserver.py --json="${PYNFS_DATA}/pynfs-block-results.json" --maketree --uid=0 --gid=0 "${EXPORT_BASE}-pnfs" block
