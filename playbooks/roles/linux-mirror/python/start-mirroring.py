58213acf09951e86958d86e98b0c4603cb204594
Luis Chamberlain <mcgrof@kernel.org>
2024-08-28 18:36:01 -0700

    linux-mirror: use 3 passes on start-mirroring.py
    start-mirroring.py is the main entry to start processing
    our mirrors.yaml when we want to start mirroring, if mirroring
    is enabled. Right now we we simply iterate over all mirrors and
    start mirroring on each one. This script already has reference
    support but it then means we put the onus on ordering of
    mirrors.yaml, which also means then that alphabetical ordering
    of the file is not possible.
    
    Instead of putting the onus of ordering by reference dependencies
    on mirrors.yaml, just have the processing of mirrors.yaml be split
    up in 3 passes:
    
     1) verification that all entries are correct
     2) mirror entries which do not have reference
     3) mirror entries which require a reference
    
    We don't have trees that have double reference, so a--> b--> c,
    we just have a--> b, in this model the above strategy works fine
    and suffices. This let's us keep alphabetical ordering on
    mirrors.yaml.
    
    Signed-off-by: Luis Chamberlain <mcgrof@kernel.org>

--- playbooks/roles/linux-mirror/python/start-mirroring.py
+++ playbooks/roles/linux-mirror/python/start-mirroring.py
@@ -20,6 +20,58 @@ default_mirrors_yaml = yaml_dir + 'mirrors.yaml'
 
 mirror_path = '/mirror/'
 
+def mirror_entry(mirror, args):
+    short_name = mirror['short_name']
+    url = mirror['url']
+    target = mirror['target']
+    reference = None
+    reference_args = []
+
+    if mirror.get('reference'):
+        reference = mirror.get('reference')
+        reference_args = [ '--reference', reference ]
+
+    if (args.verbose):
+        sys.stdout.write("\tshort_name: %s\n" % (short_name))
+        sys.stdout.write("\turl: %s\n" % (url))
+        sys.stdout.write("\ttarget: %s\n" % (url))
+        if reference is None:
+            sys.stdout.write("\treference: %s\n" % ("None"))
+        else:
+            sys.stdout.write("\treference: %s\n" % (reference))
+    cmd = [
+           'git',
+           '-C',
+           mirror_path,
+           'clone',
+           '--verbose',
+           '--progress',
+           '--mirror',
+           url,
+           target ]
+    cmd = cmd + reference_args
+    mirror_target = mirror_path + target
+    if os.path.isdir(mirror_target):
+        return
+    sys.stdout.write("Mirroring: %s onto %s\n" % (short_name, mirror_target))
+    if (args.verbose):
+        sys.stdout.write("%s\n" % (cmd))
+        sys.stdout.write("%s\n" % (" ".join(cmd)))
+    process = subprocess.Popen(cmd,
+                               stdout=subprocess.PIPE,
+                               stderr=subprocess.STDOUT,
+                               close_fds=True,
+                               universal_newlines=True)
+    try:
+        data = process.communicate(timeout=12000)
+    except subprocess.TimeoutExpired:
+        return "Timeout"
+    else:
+        process.wait()
+        if process.returncode != 0:
+            raise Exception(f"Failed clone with:\n%s" % (" ".join(cmd)))
+
+
 def main():
     parser = argparse.ArgumentParser(description='start-mirroring')
     parser.add_argument('--yaml-mirror', metavar='<yaml_mirror>', type=str,
@@ -44,6 +96,12 @@ def main():
     if (args.verbose):
         sys.stdout.write("Yaml mirror input: %s\n\n" % args.yaml_mirror)
 
+    # We do 3 passes, first to check the file has all requirements
+    # properly defined, and we bail if any of them have something missing.
+    # This avoids cloning if *any* mirror is not configured correctly.
+    #
+    # The second pass is for mirrors which do not have a reference, the
+    # third and final pass is for mirrors which do have a reference.
     total = 0
     for mirror in yaml_vars['mirrors']:
         total = total + 1
@@ -53,58 +111,17 @@ def main():
         if mirror.get('url') is None:
             raise Exception(f"Missing required url on mirror item #%d on file: %s" % (total, args.yaml_mirror))
         if mirror.get('target') is None:
-            raise Exception(f"Missing required target on mirror item #%d on file: %s" % (total, args.yaml_mirror))
+            raise Exception(f"Missing required target for mirror %s on yaml file %s on item #%d" % (mirror.get('short_name'), args.yaml_mirror, total))
 
-        short_name = mirror['short_name']
-        url = mirror['url']
-        target = mirror['target']
-        reference = None
-        reference_args = []
+    # Mirror trees without a reference first
+    for mirror in yaml_vars['mirrors']:
+        if not mirror.get('reference'):
+            mirror_entry(mirror, args)
 
+    # Mirror trees which need a reference last
+    for mirror in yaml_vars['mirrors']:
         if mirror.get('reference'):
-            reference = mirror.get('reference')
-            reference_args = [ '--reference', reference ]
-
-        if (args.verbose):
-            sys.stdout.write("Mirror #%d\n" % total)
-            sys.stdout.write("\tshort_name: %s\n" % (short_name))
-            sys.stdout.write("\turl: %s\n" % (url))
-            sys.stdout.write("\ttarget: %s\n" % (url))
-            if reference is None:
-                sys.stdout.write("\treference: %s\n" % ("None"))
-            else:
-                sys.stdout.write("\treference: %s\n" % (reference))
-        cmd = [
-               'git',
-               '-C',
-               mirror_path,
-               'clone',
-               '--verbose',
-               '--progress',
-               '--mirror',
-               url,
-               target ]
-        cmd = cmd + reference_args
-        mirror_target = mirror_path + target
-        if os.path.isdir(mirror_target):
-            continue
-        sys.stdout.write("Mirroring: %s onto %s\n" % (short_name, mirror_target))
-        if (args.verbose):
-            sys.stdout.write("%s\n" % (cmd))
-            sys.stdout.write("%s\n" % (" ".join(cmd)))
-        process = subprocess.Popen(cmd,
-                                   stdout=subprocess.PIPE,
-                                   stderr=subprocess.STDOUT,
-                                   close_fds=True,
-                                   universal_newlines=True)
-        try:
-            data = process.communicate(timeout=12000)
-        except subprocess.TimeoutExpired:
-            return "Timeout"
-        else:
-            process.wait()
-            if process.returncode != 0:
-                raise Exception(f"Failed clone with:\n%s" % (" ".join(cmd)))
+            mirror_entry(mirror, args)
 
 if __name__ == "__main__":
     main()
