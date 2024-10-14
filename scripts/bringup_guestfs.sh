3e00377af794318ef1e521429b3e0fad75651446
Daniel Gomez <da.gomez@samsung.com>
2024-10-14 16:53:09 +0200

    guestfs: bringup: fix unbound variable when debug
    Fix unbound variable errors. This is a silent error only found while
    debugging. A variable may not be set, so check if set before comparing
    the value to avoid 'unbound variable' error.
    
    ++ mktemp
    + cmdfile=/tmp/tmp.cn0hNYMZbt
    + '[' '!' -f
    /var/lib/libvirt/images/kdevops/guestfs/base_images/debian-12.raw ']'
    .//scripts/bringup_guestfs.sh: line 289:
    CONFIG_GUESTFS_HAS_CUSTOM_RAW_IMAGE: unbound variable
    make: *** [scripts/guestfs.Makefile:77: bringup_guestfs] Error 1
    
    Signed-off-by: Daniel Gomez <da.gomez@samsung.com>

--- scripts/bringup_guestfs.sh
+++ scripts/bringup_guestfs.sh
@@ -234,7 +234,8 @@ _EOT
 	# For the life of me I can't get the following line to work with
 	# the virt-builder command and so we do a full edit of the file for now
 	# edit /etc/nsswitch.conf:'s/\[!UNAVAIL=return\]//'
-	if [[ "$CONFIG_GUESTFS_DEBIAN_TRIXIE" == "y" ]]; then
+	if [[ "${CONFIG_GUESTFS_DEBIAN_TRIXIE+x}" && \
+		  "$CONFIG_GUESTFS_DEBIAN_TRIXIE" == "y" ]]; then
 		cat <<_EOT >>$cmdfile
 write /etc/nsswitch.conf: # kdevops generated /etc/nsswitch.conf
 append-line /etc/nsswitch.conf:passwd:         files
@@ -258,7 +259,8 @@ firstboot-command DEBIAN_FRONTEND=noninteractive DEBCONF_NONINTERACTIVE_SEEN=tru
 firstboot-command systemctl stop ssh
 firstboot-command systemctl start ssh
 _EOT
-		if [[ "$CONFIG_GUESTFS_COPY_SOURCES_FROM_HOST_TO_GUEST" == "y" ]]; then
+		if [[ "${CONFIG_GUESTFS_COPY_SOURCES_FROM_HOST_TO_GUEST+x}" && \
+			  "$CONFIG_GUESTFS_COPY_SOURCES_FROM_HOST_TO_GUEST" == "y" ]]; then
 		cat <<_EOT >>$cmdfile
 delete /etc/apt/sources.list.d/debian.sources
 _EOT
@@ -274,7 +276,8 @@ fi
 cmdfile=$(mktemp)
 
 if [ ! -f $BASE_IMAGE ]; then
-	if [[ "$CONFIG_GUESTFS_HAS_CUSTOM_RAW_IMAGE" == "y" ]]; then
+	if [[ "${CONFIG_GUESTFS_HAS_CUSTOM_RAW_IMAGE+x}" && \
+		  "$CONFIG_GUESTFS_HAS_CUSTOM_RAW_IMAGE" == "y" ]]; then
 		build_custom_image
 	fi
 
@@ -287,7 +290,8 @@ if [ ! -f $BASE_IMAGE ]; then
 		copy_yum_repo
 	fi
 
-	if [[ "$CONFIG_GUESTFS_COPY_SOURCES_FROM_HOST_TO_GUEST" == "y" ]]; then
+	if [[ "${CONFIG_GUESTFS_COPY_SOURCES_FROM_HOST_TO_GUEST+x}" && \
+		  "$CONFIG_GUESTFS_COPY_SOURCES_FROM_HOST_TO_GUEST" == "y" ]]; then
 		copy_host_sources
 	fi
 
@@ -342,7 +346,8 @@ do
 	TZ="$(timedatectl show -p Timezone --value)"
 	$USE_SUDO virt-sysprep -a $ROOTIMG --hostname $name --ssh-inject "kdevops:file:$SSH_KEY.pub" --timezone $TZ
 
-	if [[ "$CONFIG_LIBVIRT_ENABLE_LARGEIO" == "y" ]]; then
+	if [[ "${CONFIG_LIBVIRT_ENABLE_LARGEIO+x}" && \
+		  "$CONFIG_LIBVIRT_ENABLE_LARGEIO" == "y" ]]; then
 		lbs_idx=0
 		for i in $(seq 1 $(($CONFIG_QEMU_LARGEIO_MAX_POW_LIMIT+1))); do
 			for x in $(seq 0 $CONFIG_QEMU_EXTRA_DRIVE_LARGEIO_NUM_DRIVES_PER_SPACE); do
