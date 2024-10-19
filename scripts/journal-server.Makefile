linux-kdevops/kdevops

commit 91401166c77567bb496678901c28b52d39ce9208

journal-server: add support for 'make journal-dump'
This will let us run this for CIs so to leverage not only the
guestfs console log but another flavor in ASCII form of the systemd
journal which may be more complete and easier to read.

All we need to do now is run 'make journal-dump'

We support running this 'make journal-dump' even if you have
CONFIG_DEVCONFIG_ENABLE_SYSTEMD_JOURNAL_REMOTE disabled as its
a nice future source of kernel logs. Even if you run on the cloud
it should be possible to set this up with a central command
and control base. But we don't error our as we want all CIs to
leverage this.

Signed-off-by: Luis Chamberlain <mcgrof@kernel.org>

scripts/journal-server.Makefile
@@ -38,6 +38,10 @@ journal-status:
 journal-ls:
 	@$(Q)./workflows/kdevops/scripts/jounal-ls.sh /var/log/journal/remote/
 
+journal-dump:
+	@$(Q)./workflows/kdevops/scripts/jounal-dump.sh /var/log/journal/remote/
+
+
 journal-ln:
 	@$(Q)ansible-playbook $(ANSIBLE_VERBOSE) -l baseline,dev \
 		-f 30 -i hosts  \
@@ -55,7 +59,10 @@ journal-help:
 	@echo "journal-status	   - Ensure systemd-journal-remote works"
 	@echo "journal-ls          - List journals available and sizes"
 	@echo "journal-ln          - Add symlinks with hostnames"
+	@echo "journal-dump        - Dump journal to local journal/ directory"
 
 HELP_TARGETS += journal-help
 
 endif
+journal-dump:
+	@echo Journal disabled consider enabling CONFIG_DEVCONFIG_ENABLE_SYSTEMD_JOURNAL_REMOTE

.ci/results/fstests
@@ -1,3 +1,4 @@
 workflows/fstess/results/
 guestfs/*/*.xml
 guestfs/*/*.log
+journal/*

.ci/results/selftests
@@ -1,3 +1,4 @@
 workflows/selftests/results/
 guestfs/*/*.xml
 guestfs/*/*.log
+journal/*

workflows/kdevops/scripts/jounal-dump.sh
@@ -1,0 +1,35 @@
+#!/bin/bash
+# SPDX-License-Identifier: copyleft-next-0.3.1
+# Collect guest remote journal logs from /var/log/journal/remote/
+
+# Usage: ./collect-journals.sh <path-to-remote-journals> <ansible-inventory-file>
+DIR=$1
+INVENTORY=hosts
+JOURNAL="journal"
+
+mkdir -p journal
+
+if [[ $# -ne 1 ]]; then
+    echo "Usage: $0 <path-to-remote-journals>"
+    exit 1
+fi
+
+# Get guest names from the Ansible inventory under the [all] group
+GUESTS=$(awk '/\[all\]/ {getline; while ($0 !~ /^\[|^$/) {print $0; getline}}' $INVENTORY)
+
+# Loop through each guest and collect journal files
+for guest in $GUESTS; do
+	echo $guest
+    # Define the journal file path based on the guest
+    JOURNAL_FILE="$DIR/remote-$guest.journal"
+
+    if [[ -f $JOURNAL_FILE ]]; then
+        # Output the journal file to an artifact-friendly file
+        echo "Collecting journal for guest: $guest"
+        journalctl --file="$JOURNAL_FILE" > "$JOURNAL/$guest.journal"
+    else
+        echo "No journal file found for guest: $guest"
+    fi
+done
+
+echo "Journal collection complete."

commit e9844213433091591d5d8e1e56abc24286a23816

journal-server.Makefile: fix double target definiution by using else
Commit 91401166c77567bb ("journal-server: add support for 'make
journal-dump'") added journal-dump target but forgot to use an else
when systemd remote journal is disabled. Fix this.

Fixes: 91401166c77567bb ("journal-server: add support for 'make journal-dump'")
Signed-off-by: Luis Chamberlain <mcgrof@kernel.org>

scripts/journal-server.Makefile
@@ -63,6 +63,7 @@ journal-help:
 
 HELP_TARGETS += journal-help
 
-endif
+else
 journal-dump:
 	@echo Journal disabled consider enabling CONFIG_DEVCONFIG_ENABLE_SYSTEMD_JOURNAL_REMOTE
+endif