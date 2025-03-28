7be35be06fabb40c0c3e9c6a8b1ac3c11909c796
Chuck Lever <chuck.lever@oracle.com>
2025-03-28 12:07:07 -0400

    terraform/GCE: Bump the terraform provider version
    Update to the latest stable provider version.
    
    The null provider is not used, so it is removed.
    
    Signed-off-by: Chuck Lever <chuck.lever@oracle.com>

--- terraform/gce/provider.tf
+++ terraform/gce/provider.tf
@@ -2,11 +2,7 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "~>4.27"
-    }
-    null = {
-      source  = "hashicorp/null"
-      version = "~>2.1"
+      version = "~>5"
     }
   }
 }
