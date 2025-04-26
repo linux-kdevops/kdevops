da2fa0469be3c45b5d045cd4d992d27f83db692a
Chuck Lever <chuck.lever@oracle.com>
2025-04-26 12:16:01 -0400

    terraform/oci: Constrain the maximum provider version
    Version 7 was released recently. Do not go beyond version 6 for
    now.
    
    Signed-off-by: Chuck Lever <chuck.lever@oracle.com>

--- terraform/oci/provider.tf
+++ terraform/oci/provider.tf
@@ -1,7 +1,15 @@
 terraform {
+  required_version = ">= 0.12.6"
+  required_providers {
+    oci = {
+      source = "oracle/oci"
+      version = "~> 6"
+    }
+  }
 }
 
 provider "oci" {
+  auth                = "APIKey"
   config_file_profile = var.oci_config_file_profile
   region              = var.oci_region
 }
